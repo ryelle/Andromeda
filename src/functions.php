ae5e29499bfbb08a58ed314f734a45c77fa91d62
ryelle <kelly.dwan@gmail.com>
2015-04-30 13:04:01 -0400

    Add footer widget

--- src/functions.php
+++ src/functions.php
@@ -95,6 +95,16 @@ function andromeda_widgets_init() {
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
 	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Footer', 'andromeda' ),
+		'id'            => 'sidebar-2',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
 }
 add_action( 'widgets_init', 'andromeda_widgets_init' );
 
---
+++ src/sidebar-footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @package Andromeda
+ */
+
+if ( ! is_active_sidebar( 'sidebar-2' ) ) {
+	return;
+}
+?>
+
+<div id="tertiary" class="widget-area footer-widgets" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-2' ); ?>
+</div><!-- #secondary -->
