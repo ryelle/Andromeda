780e3d96ac1ab63c0c053208d1bb1045af9f66cc
ryelle <kelly.dwan@gmail.com>
2015-05-01 16:42:55 -0400

    comments!

--- src/comments.php
+++ src/comments.php
@@ -25,27 +25,17 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'andromeda' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+				printf( _nx( 'One comment so far', '%1$s comments so far', get_comments_number(), 'comments title', 'andromeda' ),
+					number_format_i18n( get_comments_number() ) );
 			?>
 		</h2>
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'andromeda' ); ?></h2>
-			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( __( 'Older Comments', 'andromeda' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments', 'andromeda' ) ); ?></div>
-
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // check for comment navigation ?>
 
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
 					'style'      => 'ol',
+					'avatar_size' => 50,
+					'callback'   => 'andromeda_handle_comment',
 					'short_ping' => true,
 				) );
 			?>
--- src/inc/extras.php
+++ src/inc/extras.php
@@ -46,6 +46,114 @@ function andromeda_home_template( $template ){
 }
 add_filter( 'home_template', 'andromeda_home_template' );
 
+/**
+ * Comment display callback, determine which display function should be used based on comment type.
+ * Pingbacks/trackbacks have a short display, while comments get full meta and author gravatar.
+ *
+ * @param object $comment The comment object.
+ * @param array  $args    An array of arguments.
+ * @param int    $depth   Depth of comment.
+ */
+function andromeda_handle_comment( $comment, $args, $depth ){
+	if ( 'pingback' == $comment->comment_type || 'trackback' == $comment->comment_type ) {
+		andromeda_ping( $comment, $args, $depth );
+	} else {
+		andromeda_comment( $comment, $args, $depth );
+	}
+}
+
+/**
+ * Display a comment.
+ *
+ * @param object $comment The comment object.
+ * @param array  $args    An array of arguments.
+ * @param int    $depth   Depth of comment.
+ */
+function andromeda_comment( $comment, $args, $depth ){ ?>
+
+<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
+
+<article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
+	<footer class="comment-meta">
+		<div class="comment-avatar vcard">
+			<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+		</div><!-- .comment-author -->
+
+		<div class="comment-metadata">
+			<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID, $args ) ); ?>">
+				<time datetime="<?php comment_time( 'c' ); ?>">
+					<?php printf( _x( '%1$s at %2$s', '1: date, 2: time', 'andromeda' ), get_comment_date(), get_comment_time() ); ?>
+				</time>
+			</a>
+			<?php edit_comment_link( ' &bull; ' . __( 'Edit', 'andromeda' ), '<span class="edit-link">', '</span>' ); ?>
+		</div><!-- .comment-metadata -->
+
+		<?php printf( '<p class="comment-author"><b class="fn">%s</b></p>', get_comment_author_link() ); ?>
+	</footer><!-- .comment-meta -->
+
+	<div class="comment-content">
+		<?php comment_text(); ?>
+
+		<footer class="comment-meta">
+		<?php if ( '0' == $comment->comment_approved ) : ?>
+			<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'andromeda' ); ?></p>
+		<?php endif; ?>
+		</footer><!-- .comment-meta -->
+
+	</div><!-- .comment-content -->
+
+	<?php comment_reply_link( array_merge( $args, array(
+		'add_below' => 'div-comment',
+		'depth'     => $depth,
+		'max_depth' => $args['max_depth'],
+		'before'    => '<div class="reply">',
+		'after'     => '</div>'
+	) ) ); ?>
+
+</article><!-- .comment-body -->
+<?php
+}
+
+/**
+ * Display a pingback/trackback. Only display the pinging post.
+ *
+ * @param object $comment The comment object.
+ * @param array  $args    An array of arguments.
+ * @param int    $depth   Depth of comment.
+ */
+function andromeda_ping( $comment, $args, $depth ){ ?>
+	<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
+	<article class="comment-body">
+		<svg class="avatar"><use xlink:href="#icon-pingback" /></svg>
+
+		<div class="comment-content">
+			<?php comment_author_link(); ?>
+			<footer class="comment-meta">
+				<?php if ( '0' == $comment->comment_approved ) : ?>
+				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'andromeda' ); ?></p>
+				<?php endif; ?>
+
+				<div class="comment-metadata">
+					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID, $args ) ); ?>">
+					<?php if ( strtotime( 'last week' ) < get_comment_time( 'U' ) ): // more recent than 1 week ago ?>
+						<time datetime="<?php comment_time( 'c' ); ?>">
+							<?php printf( __( '%1$s ago', 'andromeda' ), human_time_diff( get_comment_time( 'U' ) ) ); ?>
+						</time>
+					<?php else: ?>
+						<time datetime="<?php comment_time( 'c' ); ?>">
+							<?php printf( _x( '%1$s at %2$s', '1: date, 2: time', 'andromeda' ), get_comment_date(), get_comment_time() ); ?>
+						</time>
+					<?php endif; ?>
+					</a>
+					<?php edit_comment_link( __( 'Edit', 'andromeda' ), '<span class="edit-link">', '</span>' ); ?>
+				</div><!-- .comment-metadata -->
+			</footer><!-- .comment-meta -->
+		</div><!-- .comment-content -->
+	</article><!-- .comment-body -->
+<?php
+}
+
+
 if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
 	/**
 	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
