b494f025e2151842c93add0be4337a4740b1e65e
ryelle <kelly.dwan@gmail.com>
2015-06-14 14:57:25 -0400

    Remove the header from “no published posts"

--- src/partial/content-none.php
+++ src/partial/content-none.php
@@ -10,29 +10,22 @@
 
 <section class="no-results not-found">
 
-	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-		<header class="page-header">
-			<h2 class="page-title"><?php _e( 'Nothing Found', 'andromeda' ); ?></h2>
-		</header><!-- .page-header -->
+	<div class="page-content">
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-		<div class="page-content">
 			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'andromeda' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-		</div><!-- .page-content -->
 
-	<?php elseif ( is_search() ) : ?>
+		<?php elseif ( is_search() ) : ?>
 
-		<div class="page-content">
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'andromeda' ); ?></p>
 			<?php get_search_form(); ?>
-		</div><!-- .page-content -->
 
-	<?php else : ?>
+		<?php else : ?>
 
-		<div class="page-content">
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'andromeda' ); ?></p>
 			<?php get_search_form(); ?>
-		</div><!-- .page-content -->
 
-	<?php endif; ?>
+		<?php endif; ?>
+	</div><!-- .page-content -->
 
 </section><!-- .no-results -->
